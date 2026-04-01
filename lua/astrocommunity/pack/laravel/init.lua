271bc1cd0fc451bd5285720b7198c03512f41148
Serhii Shvets <wert2all@gmail.com>
2026-04-01 16:31:54 +0300

    feat(laravel): add laravel-ls

--- lua/astrocommunity/pack/laravel/init.lua
+++ lua/astrocommunity/pack/laravel/init.lua
@@ -75,7 +75,7 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "pint" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "pint", "laravel-ls" })
     end,
   },
   {
